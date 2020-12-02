af20ae1298b0b50abfea98bd5aa7e0bc6814c98f
bikcodeh <bikcodeh@gmail.com>
2020-12-02 17:47:01 -0500

    feature: configure retrofit and service for get characters
    -add class MyApplication
    -configure koin and create NetworkModule

---
+++ app/src/main/java/com/bikcode/rickandmortyapp/presentation/api/APIService.kt
@@ -0,0 +1,10 @@
+package com.bikcode.rickandmortyapp.presentation.api
+
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface APIService {
+
+    @GET(APIConstants.ENDPOINT_CHARACTER)
+    fun getCharacters(): Single<List<CharacterResponseServer>>
+}
---
+++ app/src/main/java/com/bikcode/rickandmortyapp/presentation/app/MyApplication.kt
@@ -0,0 +1,23 @@
+package com.bikcode.rickandmortyapp.presentation.app
+
+import android.app.Application
+import com.bikcode.rickandmortyapp.presentation.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MyApplication)
+            androidLogger(Level.ERROR)
+            modules(
+                listOf(
+                    networkModule
+                )
+            )
+        }
+    }
+}
